58df6685348f04dc9e482a8ec947b53e1fc9705b
Timothy <fynx626@gmail.com>
2023-03-04 23:49:16 +0100

    Added logic to retrieve arguments via safeargs\nPopulated choice chips with pickup dates.

--- app/src/main/java/com/example/carrots/OrderSpecificationsFragment.kt
+++ app/src/main/java/com/example/carrots/OrderSpecificationsFragment.kt
@@ -4,18 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.carrots.databinding.FragmentOrderSpecificationsBinding
 import com.example.carrots.model.VegViewModel
+import com.google.android.material.chip.Chip
 
 class OrderSpecificationsFragment : Fragment() {
 
     private lateinit var binding: FragmentOrderSpecificationsBinding
     private val sharedViewModel: VegViewModel by activityViewModels()
+    private var args: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        arguments?.apply {
+            args = getInt("id", 0)
+        }
     }
 
     override fun onCreateView(
@@ -24,5 +30,17 @@ class OrderSpecificationsFragment : Fragment() {
     ): View {
         binding = FragmentOrderSpecificationsBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val item = sharedViewModel.veggies[args!!]
+        binding.ivVegetable.setImageResource(item.image)
+
+        val chipList = binding.cgPickupDate
+        repeat(4){
+            index -> (chipList[index] as Chip).text = sharedViewModel.pickupDates[index]
+        }
     }
 }
