7afe2d4c0bd0e472b226ed70c6cb4b8460945b38
Timothy <fynx626@gmail.com>
2023-03-04 23:50:48 +0100

    Added logic to retrieve the pickup dates using the simpleDateFormat and Calendar

--- app/src/main/java/com/example/carrots/model/VegViewModel.kt
+++ app/src/main/java/com/example/carrots/model/VegViewModel.kt
@@ -2,10 +2,28 @@ package com.example.carrots.model
 
 import androidx.lifecycle.ViewModel
 import com.example.carrots.ui.vegArrayList
+import java.text.SimpleDateFormat
+import java.util.*
+
+const val VEGETABLE_PRICE = 3.0
 
 class VegViewModel: ViewModel() {
     private var _veggies = vegArrayList.apply { shuffle() }
     val veggies = _veggies
 
+    val pickupDates = returnPickupDates()
 
+
+
+    private fun returnPickupDates(): List<String> {
+        val options = mutableListOf<String>()
+        val formatter = SimpleDateFormat("E, MMM d", Locale.getDefault())
+        val calendar = Calendar.getInstance()
+
+        repeat(4) {
+            options.add(formatter.format(calendar.time))
+            calendar.add(Calendar.DATE, 1)
+        }
+        return options
+    }
 }
