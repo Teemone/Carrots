e06de869a9daab6e382a253022a55fb879185d88
Timothy <fynx626@gmail.com>
2023-03-06 20:13:48 +0100

    Setup action bar with navController

--- app/src/main/java/com/example/carrots/MainActivity.kt
+++ app/src/main/java/com/example/carrots/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.carrots
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private lateinit var navController: NavController
@@ -13,5 +14,11 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
         navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
