aea8a976f688ea05ea2d3b77d2d9350713b2fc67
Timothy <fynx626@gmail.com>
2023-03-07 20:19:29 +0100

    Set the recycler view to have a fixed size, to improve performance

--- app/src/main/java/com/example/carrots/ui/HomeFragment.kt
+++ app/src/main/java/com/example/carrots/ui/HomeFragment.kt
@@ -1,10 +1,10 @@
 package com.example.carrots.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.carrots.databinding.FragmentHomeBinding
@@ -36,6 +36,7 @@ class HomeFragment : Fragment() {
         binding.recyclerViewVeggies.apply {
             layoutManager = GridLayoutManager(requireContext(), SPAN_COUNT)
             adapter = VegetableListAdapter(sharedViewModel.veggies)
+            setHasFixedSize(true)
         }
 
     }
