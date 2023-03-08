3a6fd60ee5637f2d740b509e1844fd5f3682a1ab
Timothy <fynx626@gmail.com>
2023-03-08 15:41:12 +0100

    Implemented the `send order` button. It uses an implicit intent to send the order to the appropriate app to process it

--- app/src/main/java/com/example/carrots/ui/OrderSummaryFragment.kt
+++ app/src/main/java/com/example/carrots/ui/OrderSummaryFragment.kt
@@ -1,10 +1,10 @@
 package com.example.carrots.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -17,15 +17,10 @@ class OrderSummaryFragment : Fragment() {
     private lateinit var binding: FragmentOrderSummaryBinding
     private val sharedViewModel: VegViewModel by activityViewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         binding = FragmentOrderSummaryBinding.inflate(inflater)
         return binding.root
@@ -38,23 +33,47 @@ class OrderSummaryFragment : Fragment() {
         binding.orderSummary = this@OrderSummaryFragment
         binding.lifecycleOwner = viewLifecycleOwner
 
-        Toast.makeText(requireContext(),
-            "Name: ${sharedViewModel.name.value}\nPhone: ${sharedViewModel.phoneNumber.value}\nAddress: ${sharedViewModel.address.value}"
-            ,Toast.LENGTH_SHORT).show()
-
     }
 
 
     /**
      * Cancel the order and return to the home fragment
      */
-    fun cancel(){
+    fun cancel() {
         sharedViewModel.reset()
         findNavController()
             .navigate(R.id.action_orderSummaryFragment_to_homeFragment)
     }
 
-    fun sendOrder(){
+    /**
+     * Send the order to the appropriate app to process the order
+     * i.e. Gmail or any other emailing app
+     */
+
+    fun sendOrder() {
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            action = Intent.ACTION_SEND
+            type = "text/plain"
+            putExtra(
+                Intent.EXTRA_SUBJECT,
+                getString(R.string.email_subject, sharedViewModel.name.value)
+            )
+            putExtra(
+                Intent.EXTRA_TEXT, getString(
+                    R.string.email_body,
+                    sharedViewModel.vegetableName.value,
+                    sharedViewModel.quantity.value.toString(),
+                    sharedViewModel.date.value,
+                    sharedViewModel.phoneNumber.value,
+                    sharedViewModel.address.value
+                )
+            )
+        }
+
+        // Checks if the device has an appropriate app to process the intent request
+        if (activity?.packageManager?.resolveActivity(intent, 0) != null)
+            startActivity(intent)
+
 
     }
 
